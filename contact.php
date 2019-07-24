55e6284161d2f942d4c24feadddd4faf87d638e2
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2019-07-24 23:11:13 +0900

    コンタクト機能追加

--- contact.php
+++ contact.php
@@ -2,5 +2,43 @@
 
 require('./config.php');
 require("./header.php");
-echo "お問い合わせはこちらからお願いします。";
-include("./footer.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel ="stylesheet" href="../../css/top.css">
+<title>dartsarrange</title>
+</head>
+<body>
+
+<p class="lead-form">お問い合わせはこちらから！</p>
+
+<form>
+
+<div class="item">
+<label class="label">お名前</label>
+<input class="inputs" type="text" name="name">
+</div>
+
+<div class="item">
+<label class="label">メールアドレス</label>
+<input class="inputs" type="email" name="email">
+</div>
+
+<div class="item">
+<label class="label">内容</label>
+<textarea class="inputs"></textarea>
+</div>
+
+<div class="btn-area">
+<input type="submit" value="送信する"><input type="reset" value="リセット">
+</div>
+
+</form>
+
+</body>
+</html>
+
+<?php include("./footer.php"); ?>
